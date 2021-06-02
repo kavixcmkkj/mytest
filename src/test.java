8e8ff8ca7aea620c975d288a76af7dcc12d025f4
Feng Liu <www.zhangma@gmail.com>
2021-06-02 09:12:01 +0900

    tianiagongc

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author wwwzh
+ * @create 2021-06-02 9:08
+ * @desc
+ */
+public class test {
+    public static void main(String[]args){
+        System.out.println("hello");
+
+
+    }
+}
